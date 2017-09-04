4ed7eb0a6f40817f19f35918e7fba4590d7de825
sknitd <sk39693@gmail.com>
2017-09-04 10:52:37 +0530

    Add files via upload

--- SimpleExcelWriterExample.java
+++ SimpleExcelWriterExample.java
@@ -1,11 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
- 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
-
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
  
 /**
  * A very simple program that writes some data to an Excel file
@@ -16,38 +24,57 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 public class SimpleExcelWriterExample {
  
     public static void main(String[] args) throws IOException {
-        XSSFWorkbook workbook = new XSSFWorkbook();
-        XSSFSheet sheet = workbook.createSheet("Java Books");
-         
-        Object[][] bookData = {
-                {"Head First Java", "Kathy Serria", 79},
-                {"Effective Java", "Joshua Bloch", 36},
-                {"Clean Code", "Robert martin", 42},
-                {"Thinking in Java", "Bruce Eckel", 35},
-        };
- 
-        int rowCount = 0;
-         
-        for (Object[] aBook : bookData) {
-            Row row = sheet.createRow(++rowCount);
-             
-            int columnCount = 0;
-             
-            for (Object field : aBook) {
-                Cell cell = row.createCell(++columnCount);
-                if (field instanceof String) {
-                    cell.setCellValue((String) field);
-                } else if (field instanceof Integer) {
-                    cell.setCellValue((Integer) field);
-                }
-            }
-             
-        }
-         
-         
-        try (FileOutputStream outputStream = new FileOutputStream("JavaBooks.xlsx")) {
-            workbook.write(outputStream);
-        }
+    	HSSFWorkbook workbook = new HSSFWorkbook();
+    	HSSFSheet sheet = workbook.createSheet("Sample sheet");   
+    	//HSSFRow row = sheet.createRow(0);
+    	//Create a new cell in current row
+    	//Cell cell = row.createCell(0);
+    	
+    
+    	Map<String, Object[]> data = new HashMap<String, Object[]>();
+		data.put("1", new Object[] {"Emp No.", "Name", "Salary"});
+		data.put("2", new Object[] {1d, "John", 1500000d});
+		data.put("3", new Object[] {2d, "Sam", 800000d});
+		data.put("4", new Object[] {3d, "Dean", 700000d});
+		
+		Set<String> keyset = data.keySet();
+		int rownum = 0;
+		for (String key : keyset) {
+			HSSFRow row =  sheet.createRow(rownum++);
+			Object [] objArr = data.get(key);
+			int cellnum = 0;
+			for (Object obj : objArr) {
+				HSSFCell cell = row.createCell((short) cellnum++);
+				if(obj instanceof Date) 
+					cell.setCellValue((Date)obj);
+				else if(obj instanceof Boolean)
+					cell.setCellValue((Boolean)obj);
+				else if(obj instanceof String)
+					cell.setCellValue((String)obj);
+				else if(obj instanceof Double)
+					cell.setCellValue((Double)obj);
+			}
+		}
+		
+		try {
+			FileOutputStream out = 
+					new FileOutputStream(new File("/home/osgdev/Desktop/new.xls"));
+			workbook.write(out);
+			out.close();
+			System.out.println("Excel written successfully..");
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    
+    
+    
+    
+    
+    
+    
+    }
     }
  
-}
